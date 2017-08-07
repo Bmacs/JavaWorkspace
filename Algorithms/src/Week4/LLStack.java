f21a893caa1a26488be22297777826306d7b45cd
Brendan McNamara <Brendancmcnamara@gmail.com>
2017-08-07 18:06:53 -0400

    Week 4 algorithms

---
+++ Algorithms/src/Week4/LLStack.java
@@ -0,0 +1,17 @@
+package Week4;
+
+public class LLStack {
+    private java.util.LinkedList<Object> list = new java.util.LinkedList<Object>();
+
+    public LLStack() {
+
+    }
+
+    public void clear() {
+        list.clear();
+    }
+
+    public boolean isEmpty() {
+        
+    }
+}
