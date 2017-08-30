ef11c1d51afd0fd8106bd4dc19a2a37afffff54b
Brendan McNamara <Brendancmcnamara@gmail.com>
2017-08-29 23:17:54 -0400

    Decorator pattern

---
+++ MyProject/src/CoffeeWeek7/CondimentDecorator.java
@@ -0,0 +1,10 @@
+package CoffeeWeek7;
+
+public abstract class CondimentDecorator extends Beverage {
+    public Beverage beverage;
+    public abstract String getDescription();
+
+    public Size getSize() {
+        return beverage.getSize();
+    }
+}
---
+++ MyProject/src/CoffeeWeek7/DarkRoast.java
@@ -0,0 +1,11 @@
+package CoffeeWeek7;
+
+public class DarkRoast extends Beverage {
+    public DarkRoast() {
+        description = "Dark Roast Coffee";
+    }
+
+    public double cost() {
+        return .99;
+    }
+}
